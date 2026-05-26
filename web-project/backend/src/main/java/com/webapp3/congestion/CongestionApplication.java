972b65247690b552c9c21bf8cd26ae47c127205a
Kim Seok Hyeon <kbx1498@gmail.com>
2026-05-26 19:56:45 +0900

    feat: front 및 back 기본 구조 정리

---
+++ web-project/backend/src/main/java/com/webapp3/congestion/CongestionApplication.java
@@ -0,0 +1,13 @@
+package com.webapp3.congestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongestionApplication.class, args);
+	}
+
+}
---
+++ web-project/backend/src/test/java/com/webapp3/congestion/CongestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp3.congestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
